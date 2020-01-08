d8677ae8953927ca6411e86b626bc9f0480c0a0a
NKwatra <kwatranishkarsh@gmail.com>
2020-01-08 17:01:09 +0530

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+
+module.exports = {
+    "entry": "./src/index.js",
+    "output": {
+        "filename": "main.js",
+        "path": path.resolve(__dirname, "dist"),
+        "publicPath": "/dist/"
+    },
+    "module": {
+        "rules": [
+            {
+                "test": /\.js$/i,
+                "exclude": /(node_modules|bower_components)/,
+                "use": {
+                    "loader": "babel-loader"
+                }
+            },
+            {
+                "test": /\.css$/i,
+                "use": ["style-loader", "css-loader"]
+            },
+            {
+                "test": /\.(png|jpg|gif|svg)/,
+                "use": "file-loader"
+            }
+        ]
+    },
+    "mode": "development",
+    "devServer": {
+        "hot": true,
+        "port": 3000,
+        "contentBase": "./dist"
+    }
+
+}
