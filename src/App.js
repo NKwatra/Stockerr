51f5b91f6a29ee41a206cdbd4b5016cac8a187ed
NKwatra <kwatranishkarsh@gmail.com>
2020-01-09 22:23:26 +0530

    create component for logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { hot } from "react-hot-loader/root"
 import React from "react"
 import '../public/css/main.css'
-import logo from "../public/logo.png"
 import SearchBar from "./SearchBar"
 import AddButton from "./AddButton"
 import AddOverlayContainer from './containers/AddOverlayContainer'
+import Logo from "./Logo"
 
 const App = () => {
     return (
@@ -13,14 +13,11 @@ const App = () => {
             <div className="row p-3">
                 <div className="container">
                     <div className="row">
-                        <div className="col-4 text-center">
-                            <img className="logo" src={logo} />
-                        </div>
+                        <Logo />
                         <SearchBar position="float-right mt-4" offset="0" />
                     </div>
                 </div>
             </div>
-
             <AddButton />
         </div>
     );
---
+++ src/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from "../public/logo.png"
+
+export default () => {
+    return (
+        <div className="col-4 text-center">
+            <img className="logo" src={logo} />
+        </div>
+    )
+}
