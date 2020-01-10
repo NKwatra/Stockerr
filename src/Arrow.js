a71ca58c91b456d263a4e4b0c52863a73e7fd34d
NKwatra <kwatranishkarsh@gmail.com>
2020-01-10 23:51:27 +0530

    handle clicks on right arrow

--- src/Arrow.js
+++ src/Arrow.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+
+const handleClick = (event, props) => {
+    const classes = event.target.className.split(' ')
+    if (classes.indexOf("active") === -1) {
+        return;
+    }
+    if (classes.indexOf("right-arrow") != -1) {
+        // active left arrow if not
+        if (!props.leftActive) {
+            props.activateLeft();
+        }
+        // scroll right
+        const element = $(".stocks-scroller");
+        const child = element.children().eq(0);
+        const pos = element.scrollLeft() + child.width();
+        element.animate({ scrollLeft: pos }, 1000);
+        // deactivate right if required
+        if (element.width() + element.scrollLeft() + child.width() >= element[0].scrollWidth) {
+            props.deactivateRight();
+        }
+        props.updateIndex(props.index + 1)
+    }
+}
+
 export default (props) => {
+
     return (
         <div className="col-2 text-center mt-5 pt-5">
-            <span className={`${props.arrowClasses}`} />
-        </div>
+            <span className={`${props.arrowClasses} pointer`} onClick={(e) => handleClick(e, props)} />
+        </div >
     )
 }
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -22,29 +22,36 @@ const mapDispatchToProps = dispatch => {
         },
         toggleLeftArrow: () => {
             dispatch(toggleLeftActive())
+        },
+        updateIndex: (newIndex) => {
+            dispatch(updateIndex(newIndex))
         }
     }
 }
 
 class MainContainer extends React.Component {
 
+
     render() {
         const leftArrow = this.props.leftActive ? "active arrow left-arrow icon-point-left" : "inactive arrow left-arrow icon-point-left";
-        const rightArrow = this.props.rightActive ? "active arrow right-arrow icon-point-right" : "inactive arrow right arrow icon-point-right";
+        const rightArrow = this.props.rightActive ? "active arrow right-arrow icon-point-right" : "inactive arrow right-arrow icon-point-right";
         return (<div className="row mt-5 pt-5">
             <Arrow arrowClasses={`${leftArrow}`} />
             <div className="col-8 stocks-container">
-                <StockContainer loading={this.props.loading} userStocks={this.props.userStocks} data={this.props.data} />
+                <StockContainer loading={this.props.loading} userStocks={this.props.userStocks}
+                    data={this.props.data} />
             </div>
-            <Arrow arrowClasses={`${rightArrow}`} />
+            <Arrow arrowClasses={`${rightArrow}`} leftActive={this.props.leftActive} activateLeft={this.props.toggleLeftArrow}
+                deactivateRight={this.props.toggleRightArrow} index={this.props.index}
+                updateIndex={this.props./>
         </div>)
-    }
-
+        }
+    
     componentDidMount() {
         if (this.props.userStocks.length > this.props.index) {
-            this.props.toggleRightArrow()
+                this.props.toggleRightArrow()
+            }
+            }
         }
-    }
-}
-
+        
 export default connect(mapStateToProps, mapDispatchToProps)(MainContainer);
